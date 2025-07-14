1a336d878f712e2d143a56272970dfd78be41d17
MaryaTawfik <maryaahmed200412@gmail.com>
2025-07-14 09:58:44 +0300

    adjust the position of check box and delete button and add some css styles

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,27 @@ const taskContainer = document.getElementById("taskContainer");
 
 btn.addEventListener('click', function(event) {
     event.preventDefault(); 
-    if (in_t.value) {
-        const p = document.createElement('p');
-        p.textContent = in_t.value;
-        const check=document.createElement('input');
-        
+    if (in_t.value) {const taskRow = document.createElement('div');
+        taskRow.className = 'task-row';
+
+        const check = document.createElement('input');
         check.type = 'checkbox';
-        const del= document.createElement('button');
-        del.textContent='❌';
-        p.appendChild(del);
-        p.appendChild(check);
-        taskContainer.appendChild(p); 
+
+        const taskText = document.createElement('p');
+        taskText.textContent = in_t.value;
+
+        const del = document.createElement('button');
+        del.textContent = '✖';
+
+        taskRow.appendChild(check);
+        taskRow.appendChild(taskText);
+        taskRow.appendChild(del);
+        taskContainer.appendChild(taskRow); 
+
         in_t.value = ''; 
+        
+       
+        
         check.addEventListener('click',function()
     {
         if (check.checked) {
@@ -25,7 +34,7 @@ btn.addEventListener('click', function(event) {
                 p.style.textDecoration = 'none';} 
 });
         del.addEventListener('click',function(){
-            taskContainer.removeChild(p);
+            taskContainer.removeChild(taskRow);
         }
      );
     }
